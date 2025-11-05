ebc9ebd695ba9a04173faa6e4cebebd7b66fb1cd
harsh25csu389-ui <harsh25csu389@ncuindia.edu>
2025-11-05 15:56:29 +0530

    Q12.c

--- Q12.c
+++ Q12.c
@@ -9,24 +9,22 @@ int main() {
         return 1; 
     }
 
-    int max_score = scores[0];
-    int min_score = scores[0];
+    int max = scores[0];
+    int min = scores[0];
 
     for (int i = 1; i < n; i++) {
-        if (scores[i] > max_score) {
-            max_score = scores[i];
+        if (scores[i] > max) {
+            max = scores[i];
         }
-        if (scores[i] < min_score) {
-            min_score = scores[i];
+        if (scores[i] < min) {
+            min = scores[i];
         }
     }
 
-    printf("--- Score Analysis Report ---\n");
-    printf("Total scores analyzed: %d\n", n);
-    printf("-----------------------------\n");
+  
     printf("Maximum Score (Highest Rank): %d\n", max_score);
     printf("Minimum Score (Lowest Rank): %d\n", min_score);
-    printf("-----------------------------\n");
-
+  
     return 0;
 }
+
