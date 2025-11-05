493df891aac14a9b61714f401ec3f7d73cc5d48c
harsh25csu389-ui <harsh25csu389@ncuindia.edu>
2025-11-05 19:02:37 +0530

    Q14.c

--- Q14.c
+++ Q14.c
@@ -5,11 +5,11 @@ int main() {
     int arr[] = {17, 2, 4, 11, 20, 19, 1, 0, 7, 31, 15, 6};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    int primeCount = 0;
+    int count= 0;
     int i;          
     int j;          
     int num;        
-    int isPrime;    
+    int c;    
 
     printf("Input Array: {");
     for (i = 0; i < n; i++) {
@@ -19,28 +19,28 @@ int main() {
 
     for (i = 0; i < n; i++) {
         num = arr[i];
-        isPrime = 1; 
+        c = 1; 
 
         if (num <= 1) {
-            isPrime = 0;
+            c = 0;
         }
         else {
             
             for (j = 2; j <= num / 2; j++) {
                 if (num % j == 0) {
-                    isPrime = 0; 
+                    c = 0; 
                     break;       
                 }
             }
         }
 
-        if (isPrime == 1) {
-            primeCount++;
+        if (c == 1) {
+         count++;
         }
     }
-    printf("--------------------------------------\n");
-    printf("Total number of prime numbers found: %d\n", primeCount);
-    printf("--------------------------------------\n");
-
+   
+    printf("Total number of prime numbers found: %d\n", count);
+   
     return 0;
+
 }
